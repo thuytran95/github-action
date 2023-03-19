10698e95dd483c7a818e91421aaca1eb47744152
thuytran95 <thuytt0295@gmail.com>
2023-03-19 07:01:10 +0700

    add custom js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,12 @@
+import core from '@actions/core'
+import github from '@actions/github';
+import exec from '@actions/exec'
+
+
+function run (){
+	core.notice('Hello custom javascript action')
+
+}
+
+
+run()
